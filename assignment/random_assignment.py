3472c3554fcc0b477055e9e6d45979a30d2c0f37
Francis Deh <dfdemajesty699@gmail.com>
2022-03-18 18:55:28 +0000

    added extra assignment

--- assignment/random_assignment.py
+++ assignment/random_assignment.py
@@ -41,3 +41,54 @@ def speak_to_aliens(sentence):
 
 
 print(speak_to_aliens("The greatest victory is that which requires no battle"))
+
+"""
+## B5. Padding
+
+Your colleague has recently started ice-skating, and after an injury, has become obsessed with padding his clothes in order to prevent future injuries. His padding obsession has become so severe, that all the documents you write together must also be padded. 
+
+For every sentence, each word must be padded with enough spaces on each side, that each word has the same amount of characters as the longest word in that sentence.
+
+```
+'let's pad this sentence'
+'  let's    pad    this  sentence'
+```"""
+
+
+def apply_padding(sentence):
+    max_length = 0
+    split_sentence = sentence.split(" ")
+    for word in split_sentence:
+        if len(word) > max_length:
+            max_length = len(word)
+
+    new_sentence = ''
+    for word in split_sentence:
+        if len(word) < max_length:
+            new_sentence += " " * (max_length - len(word)) + word
+        else:
+            new_sentence += " " + word
+    return new_sentence
+
+
+print("let's pad this sentence")
+print(apply_padding("let's pad this sentence"))
+
+
+"""
+## B1. Password Parsing
+
+Your task is to create a solution which checks if a given password is valid or invalid, based on a policy.
+
+For example, suppose you have the following inputs:
+
+```
+1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc
+```
+
+Each input gives the **password policy** and then the **password**. The password policy indicates the lowest and highest number of times a given letter must appear for the password to be valid. For example, `1-3 a` means that the password must contain `a` at least `1` time and at most `3` times.
+
+In the above example, *`2`* passwords are valid. The middle password, `cdefg`, is not; it contains no instances of `b`, but needs at least `1`. The first and third passwords are valid: they contain one `a` or nine `c`, both within the limits of their respective policies.
+"""
