ce4beac25def866ed417af3132cf763e376bc32b
Francis Deh <dfdemajesty699@gmail.com>
2022-03-18 06:23:29 +0000

    brief description

--- sorting/quicksort.py
+++ sorting/quicksort.py
@@ -2,6 +2,8 @@ from random import randint
 
 
 def qsort(ls):
+    """ Worst case if you pick the pivot in first index for an already sorted array. Much faster if middle is picked
+    for sorted array. O(nlogn) """
     if len(ls) < 2: return ls
     pivot = ls[0]
     less = [x for x in ls[1:] if x <= pivot]
